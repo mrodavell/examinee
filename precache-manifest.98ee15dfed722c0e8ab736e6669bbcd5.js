7c097377afa8b1ee612e45545a108b763fd7a03c
mrodavell <jomar.llevado@ustp.edu.ph>
2020-11-09 21:38:38 +0800

    Edited meta

---
+++ precache-manifest.98ee15dfed722c0e8ab736e6669bbcd5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fc7f219a16c3f1af0a8b8b15ae97bd1",
+    "url": "/examinee/index.html"
+  },
+  {
+    "revision": "8f0dc7f361867c009f42",
+    "url": "/examinee/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8f7f76c852f99296edb4",
+    "url": "/examinee/static/css/main.8e18f612.chunk.css"
+  },
+  {
+    "revision": "8f0dc7f361867c009f42",
+    "url": "/examinee/static/js/2.9387ca07.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/examinee/static/js/2.9387ca07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f7f76c852f99296edb4",
+    "url": "/examinee/static/js/main.df9560e9.chunk.js"
+  },
+  {
+    "revision": "6553a5920463ac28c1fa",
+    "url": "/examinee/static/js/runtime-main.436e07e0.js"
+  },
+  {
+    "revision": "bdddb4d4999ce1cb8ce56e4e0bf50b83",
+    "url": "/examinee/static/media/bg.bdddb4d4.jpg"
+  },
+  {
+    "revision": "0dad67f1333e8b775dad390ccf2c6b79",
+    "url": "/examinee/static/media/logo.0dad67f1.png"
+  },
+  {
+    "revision": "a6cbd833e4f2ce6fd49fafeb67000fe4",
+    "url": "/examinee/static/media/prc.a6cbd833.png"
+  }
+]);
